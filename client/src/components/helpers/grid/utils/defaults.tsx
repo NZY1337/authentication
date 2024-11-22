99b1525f380ab05bcb0a7781b171a63397116b2a
lvb5mh5tfkwptyu3knyxx5s5qejk7enze7gix43wut2v6v52pk4q <MOCANA04@heiway.net>
2024-11-22 23:38:48 +0200

    wip

---
+++ client/src/components/helpers/grid/utils/defaults.tsx
@@ -0,0 +1,31 @@
+export const defaultStyles = {
+    image: {
+        width: '100%',
+        height: '200px',
+        objectFit: 'cover',
+        borderRadius: '5px',
+    },
+    title: {
+        color: 'orange',
+        pt: 3,
+    },
+    subtitle: {
+        color: 'lightgray',
+        pt: 3,
+    },
+    description: {
+        color: 'lightgray',
+        mt: 1,
+    },
+    button: {
+        backgroundColor: 'black',
+        color: 'orange',
+        borderColor: 'black',
+        fontWeight: 'bold',
+        '&:hover': {
+            color: 'orange',
+            borderColor: 'black',
+            opacity: '.8',
+        },
+    },
+};
